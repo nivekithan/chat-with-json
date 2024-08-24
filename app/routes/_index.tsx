96f550b1e6cb459af06d5376ac542b02dba523f1
Nivekithan S <nivekithan363@gmail.com>
2024-08-24 11:50:41 +0530

    Add modal for uploading json file

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,82 @@
 import type { MetaFunction } from "@remix-run/cloudflare";
+import React, { useState } from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card";
+import { Input } from "~/components/ui/input";
+import { Label } from "~/components/ui/label";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
+    { title: "Chat with JSON" },
     {
       name: "description",
-      content: "Welcome to Remix on Cloudflare!",
+      content: "Chat with json",
     },
   ];
 };
 
+type JsonContent =
+  | {
+      type: "error";
+      error: string;
+    }
+  | null
+  | { type: "success"; content: unknown };
 export default function Index() {
-  return (
-    <div className="font-sans p-4">
-      <h1 className="text-3xl">Welcome to Remix on Cloudflare</h1>
-      <ul className="list-disc mt-4 pl-6 space-y-2">
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/docs"
-            rel="noreferrer"
-          >
-            Remix Docs
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://developers.cloudflare.com/pages/framework-guides/deploy-a-remix-site/"
-            rel="noreferrer"
-          >
-            Cloudflare Pages Docs - Remix guide
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
+  const [jsonContent, setJsonContent] = useState<JsonContent>(null);
+
+  async function onUploadFile(e: React.ChangeEvent<HTMLInputElement>) {
+    const files = e.currentTarget.files;
+
+    if (!files || files.length === 0) {
+      setJsonContent({ type: "error", error: "No file selected" });
+      return;
+    }
+
+    const file = files[0];
+
+    const content = await file.text();
+
+    try {
+      const json = JSON.parse(content);
+      setJsonContent({ type: "success", content: json });
+    } catch (error) {
+      if (error instanceof Error) {
+        setJsonContent({
+          type: "error",
+          error: "Invalid json. Recheck the file",
+        });
+      }
+    }
+  }
+
+  if (jsonContent === null || jsonContent.type === "error") {
+    return (
+      <div className="grid place-items-center min-h-screen">
+        <Card>
+          <CardHeader>
+            <CardTitle>Chat with JSON</CardTitle>
+          </CardHeader>
+          <CardContent className="flex flex-col gap-y-3">
+            <Label htmlFor="file">Upload JSON file</Label>
+            <Input
+              type="file"
+              name="file"
+              id="file"
+              onChange={onUploadFile}
+              accept=".json"
+            />
+            <p className="text-error">{jsonContent?.error}</p>
+          </CardContent>
+        </Card>
+      </div>
+    );
+  } else {
+    return <pre>{JSON.stringify(jsonContent.content, null, 2)}</pre>;
+  }
 }
